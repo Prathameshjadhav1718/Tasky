f85fabf6468b37057092f5a6e23c5b48ef3512ee
PrathameshJadhav1718 <prathameshjadhav1718@gmail.com>
2021-07-04 12:49:32 +0530

    connenteditable

---
+++ index.js
@@ -0,0 +1,135 @@
+// 📌Parent element to store cards
+const taskContainer = document.querySelector(".task__container");
+
+// 📌Global Store
+let globalStore = [];
+
+const newCard = ({
+  id,
+  imageUrl,
+  taskTitle,
+  taskDescription,
+  taskType,
+}) => `<div class="col-md-6 col-lg-4" id=${id}>
+<div class="card">
+  <div class="card-header d-flex justify-content-end gap-2">
+    <button type="button" id=${id} class="btn btn-outline-success" onclick="editCard.apply(this, arguments)">
+      <i class="fas fa-pencil-alt" id=${id} onclick="editCard.apply(this, arguments)"></i>
+    </button>
+    <button type="button" id=${id} class="btn btn-outline-danger" onclick="deleteCard.apply(this, arguments)">
+      <i class="fas fa-trash-alt" id=${id} onclick="deleteCard.apply(this, arguments)"></i>
+    </button>
+  </div>
+  <img
+    src=${imageUrl}
+    class="card-img-top"
+    alt="..."
+  />
+  <div class="card-body">
+    <h5 class="card-title">${taskTitle}</h5>
+    <p class="card-text">
+      ${taskDescription}
+    </p>
+    <span class="badge bg-primary">${taskType}</span>
+  </div>
+  <div class="card-footer text-muted">
+    <button type="button" class="btn btn-outline-primary float-end">
+      Open Task
+    </button>
+  </div>
+</div>
+</div>`;
+
+const loadInitialTaskCards = () => {
+  // 📌 To Access localstorage
+  const getInitialData = localStorage.getItem("tasky"); 
+  if (!getInitialData) return;
+
+  // 📌 To Convert stringified-object to object
+  const { cards } = JSON.parse(getInitialData);
+
+  // 📌Map around the array to generate HTML card and inject it to DOM
+  cards.map((cardObject) => {
+    const createNewCard = newCard(cardObject);
+    taskContainer.insertAdjacentHTML("beforeend", createNewCard);
+    globalStore.push(cardObject);
+  });
+};
+
+const updateLocalStorage = () =>
+  localStorage.setItem("tasky", JSON.stringify({ cards: globalStore }));
+
+const saveChanges = () => {
+  const taskData = {
+    id: `${Date.now()}`, // 📌To Generate unique number for card id
+    imageUrl: document.getElementById("imageurl").value,
+    taskTitle: document.getElementById("tasktitle").value,
+    taskType: document.getElementById("tasktype").value,
+    taskDescription: document.getElementById("taskdescription").value,
+  };
+
+  // 📌HTML code
+  const createNewCard = newCard(taskData);
+
+  taskContainer.insertAdjacentHTML("beforeend", createNewCard);
+  globalStore.push(taskData);
+
+  // 📌To Add to localstorage
+  updateLocalStorage();
+};
+
+const deleteCard = (event) => {
+  // 📌id
+  event = window.event;
+  const targetID = event.target.id;
+  const tagname = event.target.tagName; // 📌BUTTON
+
+  // 📌Search the globalStore, remove the object which matches with the id
+  globalStore = globalStore.filter((cardObject) => cardObject.id !== targetID);
+
+  updateLocalStorage();
+
+  // 📌To Access DOM to remove them
+
+  if (tagname === "BUTTON") {
+    // 📌task__container
+    return taskContainer.removeChild(
+      event.target.parentNode.parentNode.parentNode 
+    );
+  }
+
+  // 📌task__container
+  return taskContainer.removeChild(
+    event.target.parentNode.parentNode.parentNode.parentNode 
+  );
+};
+
+// 📌Card Edit
+const editCard = (event) => {
+  event = window.event;
+  const targetID = event.target.id;
+  const tagname = event.target.tagName;
+
+  let parentElement;
+
+  if(tagname === "BUTTON"){
+    parentElement = event.target.parentNode.parentNode;
+  }else{
+    parentElement = event.target.parentNode.parentNode.parentNode;
+  }
+
+
+  let taskTitle = parentElement.childNodes[5].childNodes[1];
+  let taskDescription  = parentElement.childNodes[5].childNodes[3];
+  let taskType  = parentElement.childNodes[5].childNodes[5];
+  let submitButton  = parentElement.childNodes[7].childNodes[1]
+  
+  //📌setAttribute Method
+  taskTitle.setAttribute("contenteditable", "true");
+  taskDescription.setAttribute("contenteditable", "true");
+  taskType.setAttribute("contenteditable", "true");
+  submitButton.innerHTML = "Save Changes";
+}; 
+
+
+
